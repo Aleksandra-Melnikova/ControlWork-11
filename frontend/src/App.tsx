bb7df6f2945825ef1b684a2c780b982cf8ba034f
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2025-01-25 13:35:29 +0600

    [API] Create RegisterPage, LoginPage. Prescribed slices and thunk for user.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
 import './App.css'
+import Layout from './components/UI/Layout/Layout.tsx';
+import { Route, Routes } from 'react-router-dom';
+import RegisterPage from './features/users/RegisterPage.tsx';
+import LoginPage from './features/users/LoginPage.tsx';
+
 
 const App = () => {
 
   return (
     <>
-
+      <Layout>
+        <Routes>
+          {/*<Route path="/" element={<Product />} />*/}
+          {/*<Route path="/products" element={<Product />} />*/}
+          {/*<Route path="/add_products" element={<ProductsForm />} />*/}
+          {/*<Route path="/products/:productsId" element={<DetailedProducts />} />*/}
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="*"
+            element={<h1 className={"text-center mt-5"}>Not found</h1>}
+          />
+        </Routes>
+      </Layout>
     </>
   )
 };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { persistor, store } from "./app/store.ts";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+);
